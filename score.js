2dd4c9c3ef6eb15947f45f2a0f8b1363c4f452d0
Ilja Wieselmann <ilja.wie@gmail.com>
2020-01-31 15:53:18 +0100

    to go

---
+++ score.js
@@ -0,0 +1,47 @@
+var p1Button = document.querySelector("#p1");
+var p2button = document.getElementById("p2");
+var resetButton = document.getElementById("reset");
+var p1Display= document.querySelector("#p1Display");
+var p2Display= document.querySelector("#p2Display");
+var numInput= document.querySelector("input");
+var winningScoreDisplay= document.querySelector("p span");
+var p1Score = 0;
+var p2Score = 0;
+var gameState = false;
+var winningscore = 5;
+
+console.log(winningScoreDisplay);
+
+p1Button.addEventListener("click", function() {
+	if(!gameState){
+		p1Score++;
+		if (p1Score===winningscore) {gameState=true; p1Display.classList.add("winner");}
+		p1Display.textContent =p1Score;};
+});
+
+p2button.addEventListener("click", function (){
+	if(!gameState){
+		p2Score++;
+		if (p2Score===winningscore) {gameState=true; p2Display.classList.add("winner");}
+		p2Display.textContent =p2Score;};
+});
+
+resetButton.addEventListener("click", reset());
+
+function reset(){
+	p1Score=0;
+	p2Score=0;
+	p1Display.textContent=0;
+	p2Display.textContent=0;
+	p1Display.classList.remove("winner");
+	p2Display.classList.remove("winner");
+	gameState =false;
+	console.log("reset");
+}
+
+numInput.addEventListener("change", function (){
+console.log(numInput.value);
+winningscore=parseInt(numInput.value);
+winningScoreDisplay.textContent = numInput.value;
+reset();
+});
